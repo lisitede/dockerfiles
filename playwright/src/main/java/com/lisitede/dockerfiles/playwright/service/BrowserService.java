c156019a1a31ee17122993a8882146e0ad120132
hbrls <shuaizhexu@gmail.com>
2025-07-09 17:51:43 +0800

    A: build playwright

--- playwright/src/main/java/com/lisitede/dockerfiles/playwright/service/BrowserService.java
+++ playwright/src/main/java/com/lisitede/dockerfiles/playwright/service/BrowserService.java
@@ -5,12 +5,14 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.PlaywrightException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.Paths;
 
 
+@Slf4j
 @Service
 public class BrowserService {
 
@@ -18,12 +20,15 @@ public class BrowserService {
     private String executable;
 
     public String version() {
+        log.warn(executable);
+        
         try (Playwright playwright = Playwright.create();
              Browser browser = playwright.chromium()
                      .launch(new BrowserType.LaunchOptions().setExecutablePath(Paths.get(executable)))) {
             String version = browser.version();
             return version;
         } catch (PlaywrightException e) {
+            e.printStackTrace();
             throw new HttpServerException(-1, "Playwright: " + e.getMessage());
         }
     }
